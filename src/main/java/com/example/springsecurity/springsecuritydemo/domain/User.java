0958e2d09049cea5ebeb2bbaa6092b4900d54e78
ILoveStudyAndWork <fengyi_ling@qq.com>
2021-08-09 14:25:48 +0800

    refactor: attempt to use lombok

--- src/main/java/com/example/springsecurity/springsecuritydemo/domain/User.java
+++ src/main/java/com/example/springsecurity/springsecuritydemo/domain/User.java
@@ -1,5 +1,8 @@
 package com.example.springsecurity.springsecuritydemo.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,36 +14,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Setter
+@Getter
 public class User {
-    private Long id;
+    private long id;
     private String username;
     private String password;
     private Set<Authorities> authorities = new HashSet<>();
 
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-    public Long getId() {
+    public long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    
     @OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL, mappedBy="user")
     public Set<Authorities> getAuthorities() {
         return authorities;
-    }
-    public void setAuthorities(Set<Authorities> authorities) {
-        this.authorities = authorities;
     }
 }
