a5c2ec11f3e6f6ca2278bc241820a2ab994fbd8f
ILoveStudyAndWork <fengyi_ling@qq.com>
2021-08-10 13:24:50 +0800

    feat:use AuthenticationPrincipal

--- src/main/java/com/example/springsecurity/springsecuritydemo/controller/DashBoardController.java
+++ src/main/java/com/example/springsecurity/springsecuritydemo/controller/DashBoardController.java
@@ -1,12 +1,17 @@
 package com.example.springsecurity.springsecuritydemo.controller;
 
+import com.example.springsecurity.springsecuritydemo.domain.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashBoardController {
     @GetMapping("/dashboard")
-    public String getDashBoard() {
+    public String getDashBoard(@AuthenticationPrincipal User user, ModelMap model) {
+        model.put("user", user);
+        System.out.println(user);
         return "dashboard";
     }
 }
